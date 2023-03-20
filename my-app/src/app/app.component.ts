157d3cc487fbe8017c76391955e457945dfc73fe
juniorsilva0952 <juniorsilva050752@gmail.com>
2023-03-20 16:50:50 -0400

    meu componente

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  nome= 'wilton';
+  count=10;
+
+ menos(){
+  this.count = this.count--;
 }
+
+ mais(){
+this.count = this.count++;
+  
+}
+     } 
